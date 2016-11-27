69c339c35b7bc6a94b17dfd9eaf57c2e84c8f970
Florian Heck <florian.heck@hotmail.de>
2016-11-27 12:15:42 +0100

    Updated compiler versions

---
+++ conf/evolutions/default/30.sql
@@ -0,0 +1,32 @@
+# DEFAULT SCHEMA
+
+# --- !Ups
+UPDATE e_prog_lang SET name = 'C, GCC 4.9.3, Standard: C11, gegen libm gelinkt' WHERE LANGUAGE = 'C';
+UPDATE e_prog_lang SET name = 'COBOL, OpenCOBOL 1.1, Free Format' WHERE LANGUAGE = 'COBOL';
+UPDATE e_prog_lang SET name = 'C++, GCC 4.9.3, Standard: C++11, gegen libm gelinkt' WHERE LANGUAGE = 'CPP';
+UPDATE e_prog_lang SET name = 'C#, Mono C# compiler version 2.10.9, Mono Project' WHERE LANGUAGE = 'CSHARP';
+UPDATE e_prog_lang SET name = 'D, version 0.17.1-r1, LDC: DMD v2.068.2 and LLVM 3.8.1' WHERE LANGUAGE = 'D';
+UPDATE e_prog_lang SET name = 'Forth, Gforth 0.7.3' WHERE LANGUAGE = 'FORTH';
+UPDATE e_prog_lang SET name = 'Fortran, GCC 4.9.3, Standard: Fortran 90' WHERE LANGUAGE = 'FORTRAN';
+UPDATE e_prog_lang SET name = 'FreePascal, fpc 2.4.6' WHERE LANGUAGE = 'FREEPASCAL';
+UPDATE e_prog_lang SET name = 'Go, Go 1.7.3' WHERE LANGUAGE = 'GO';
+UPDATE e_prog_lang SET name = 'Haskell, The Haskell Platform 2013.2.0.0-r2, ghc 7.10.3' WHERE LANGUAGE = 'HASKELL';
+UPDATE e_prog_lang SET name = 'Intercal, C-INTERCAL 30.0' WHERE LANGUAGE = 'INTERCAL';
+UPDATE e_prog_lang SET name = 'Java, Oracle Java SE Development Kit 1.8.0.112' WHERE LANGUAGE = 'JAVA';
+UPDATE e_prog_lang SET name = 'JavaScript, Node.js 4.6.1' WHERE LANGUAGE = 'JAVASCRIPT';
+UPDATE e_prog_lang SET name = 'Lua, Lua 5.1.5-r3' WHERE LANGUAGE = 'LUA';
+UPDATE e_prog_lang SET name = 'MMIX, MMIX 20131017' WHERE LANGUAGE = 'MMIX';
+UPDATE e_prog_lang SET name = 'OCaml, OCaml 4.02.3' WHERE LANGUAGE = 'OCAML';
+UPDATE e_prog_lang SET name = 'Objective C, GCC 4.9.3, Standard: C99, gegen libm gelinkt, kompiliert mit -lobjc' WHERE LANGUAGE = 'OBJECTIVEC';
+UPDATE e_prog_lang SET name = 'Perl, Perl 5.22.2' WHERE LANGUAGE = 'PERL';
+UPDATE e_prog_lang SET name = 'PHP, PHP 5.6.28-pl0, Zend Engine v2.6.0, with Zend OPcache v7.0.6-dev' WHERE LANGUAGE = 'PHP';
+UPDATE e_prog_lang SET name = 'Python 2, Python 2.7.12' WHERE LANGUAGE = 'PYTHON2';
+UPDATE e_prog_lang SET name = 'Python 3, Python 3.4.5' WHERE LANGUAGE = 'PYTHON3';
+UPDATE e_prog_lang SET name = 'Ruby, Ruby 2.1.9p490' WHERE LANGUAGE = 'RUBY';
+UPDATE e_prog_lang SET name = 'Rust, Rust 1.13.0' WHERE LANGUAGE = 'RUST';
+UPDATE e_prog_lang SET name = 'Scala, Scala 2.11.8' WHERE LANGUAGE = 'SCALA';
+UPDATE e_prog_lang SET name = 'Tool Command Language, TCL 8.5.17' WHERE LANGUAGE = 'TCL';
+UPDATE e_prog_lang SET name = 'Vala, Vala 0.32.1' WHERE LANGUAGE = 'VALA';
+UPDATE e_prog_lang SET name = 'Visual Basic .Net, vbnc 2.10, Mono Project' WHERE LANGUAGE = 'VISUALBASIC';
+
+# --- !Downs
