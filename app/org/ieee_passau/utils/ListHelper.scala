f0b811ac8ca630b6651a0486465e214ab104000b
Florian Heck <florian.heck@hotmail.de>
2017-11-28 19:51:28 +0100

    fix classification of solution result

--- app/org/ieee_passau/utils/ListHelper.scala
+++ app/org/ieee_passau/utils/ListHelper.scala
@@ -11,23 +11,35 @@ object ListHelper {
 
     solutionList.view.groupBy(_._1).map { case (s, testruns) =>
       (s, testruns.map {
-        case (solution, testcase, testrun) => (testcase, testrun)
+        case (_, testcase, testrun) => (testcase, testrun)
       }.sortBy(_._1.position))
     }.toList.view.sortBy(_._1.created)(Ordering[Date])
       .zipWithIndex.map {
       case ((solution, caseruns), pos) =>
-        val solved = caseruns.forall { case (testcase, testrun) => testrun.result == Passed }
-        val failed = caseruns.exists { case (testcase, testrun) => testrun.result != Passed && testrun.result != Queued }
-        val canceled = caseruns.forall { case (testcase, testrun) => testrun.result == Canceled || testrun.result == Passed }
-        val queued = caseruns.exists { case (testcase, testrun) => testrun.result == Queued || testrun.stage.isDefined}
-        val result = if (queued)
-          Queued
-        else if (canceled && !solved)
-          Canceled
-        else if (failed)
-          WrongAnswer
-        else
+
+        val result = if (
+          caseruns.forall { case (_, testrun) => testrun.result == Passed })
           Passed
+        else if (
+          caseruns.exists { case (_, testrun) => testrun.result == Queued || testrun.stage.isDefined })
+          Queued
+        else if (
+          caseruns.forall { case (_, testrun) => testrun.result == Canceled || testrun.result == Passed })
+          Canceled
+        else if(
+          caseruns.exists { case (_, testrun) => testrun.result == CompileError })
+          CompileError
+        else if(
+          caseruns.exists { case (_, testrun) => testrun.result == ProgramError })
+          ProgramError
+        else if(
+          caseruns.exists { case (_, testrun) => testrun.result == RuntimeExceeded })
+          RuntimeExceeded
+        else if(
+          caseruns.exists { case (_, testrun) => testrun.result == MemoryExceeded })
+          MemoryExceeded
+        else
+          WrongAnswer
 
         SolutionListEntry(pos + 1, result, solution,
           caseruns.map { case (tcase, trun) =>
