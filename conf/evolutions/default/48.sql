b5fbd26ee19fdc1b58058627244146800220d706
Florian Heck <florian.heck@hotmail.de>
2018-11-09 12:52:48 +0100

    update evolution with score calculation

--- conf/evolutions/default/48.sql
+++ conf/evolutions/default/48.sql
@@ -6,6 +6,18 @@ SCHEMA
 
 ALTER TABLE solutions ADD COLUMN score INT NOT NULL DEFAULT 0;
 
+UPDATE solutions
+SET score = tc.sumPoints
+FROM (
+       SELECT r.solution_id, sum(c.points) sumPoints
+       FROM testcases c,
+            testruns r
+       WHERE r.testcase_id = c.id
+         AND r.result = 'PASSED'
+       GROUP BY r.solution_id
+     ) tc
+WHERE tc.solution_id = id;
+
 # --- !Downs
 
 ALTER TABLE solutions DROP COLUMN score;
