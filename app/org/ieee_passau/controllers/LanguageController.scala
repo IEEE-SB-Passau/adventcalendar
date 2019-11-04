59b0619fbb8c55f4179428af5de99ac423b2959e
Mat2095 <Matthias-Bay@t-online.de>
2019-11-04 20:36:34 +0100

    Revert "codelang: add id to edit-page"
    This reverts commit 191141e0

--- app/org/ieee_passau/controllers/LanguageController.scala
+++ app/org/ieee_passau/controllers/LanguageController.scala
@@ -60,25 +60,20 @@ class LanguageController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
 
   def update(language: String): Action[AnyContent] = requirePermission(Admin) { implicit admin => Action.async { implicit rs =>
     Languages.byLang(language).flatMap {
-      case Some(lang) =>
-        // If the id was changed, check if it is available.
-        var filledForm = languageUpdateForm.bindFromRequest()
-        val idValue = filledForm.apply("id").value.get
-        if (idValue != language && !Languages.idAvailable(idValue)) {
-          filledForm = filledForm.withError("id", "codelang.id.error.taken")
-        }
-        filledForm.fold(
-          errorForm => {
-            Future.successful(BadRequest(org.ieee_passau.views.html.language.edit(lang.id, errorForm)))
-          },
+      case Some(lang) => languageUpdateForm.bindFromRequest.fold(
+        errorForm => {
+          Future.successful(BadRequest(org.ieee_passau.views.html.language.edit(lang.id, errorForm)))
+        },
 
-          codelang => {
-            db.run(Languages.update(language, codelang)).map(_ =>
-              Redirect(org.ieee_passau.controllers.routes.LanguageController.edit(codelang.id))
-                .flashing("success" -> rs.messages("codelang.update.message", codelang.id))
-            )
-          }
-        )
+        codelang => {
+          val newCodelang = lang.copy(name = codelang.name, highlightClass = codelang.highlightClass, extension = codelang.extension,
+            cpuFactor = codelang.cpuFactor, memFactor = codelang.memFactor, comment = codelang.comment, active = codelang.active)
+          db.run(Languages.update(language, newCodelang)).map(_ =>
+            Redirect(org.ieee_passau.controllers.routes.LanguageController.edit(language))
+              .flashing("success" -> rs.messages("codelang.update.message", codelang.id))
+          )
+        }
+      )
       case _ => Future.successful(NotFound(org.ieee_passau.views.html.errors.e404()))
     }
   }}
@@ -99,7 +94,6 @@ class LanguageController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
 
   val languageUpdateForm = Form(
     mapping(
-      "id" -> nonEmptyText(maxLength = 30),
       "name" -> nonEmptyText(maxLength = 196).verifying(pattern(""".*, .*""".r, error = "codelang.name_and_version.error.pattern")),
       "highlightClass" -> text(maxLength = 30),
       "extension" -> nonEmptyText(maxLength = 10),
@@ -107,7 +101,7 @@ class LanguageController @Inject()(val dbConfigProvider: DatabaseConfigProvider,
       "memFactor" -> of[Float],
       "comment" -> text,
       "active" -> boolean
-    )((id, name, highlightClass, extension, cpuFactor, memFactor, comment, active) => Language(id, name, highlightClass, extension, cpuFactor, memFactor, comment, active))
-    ((l: Language) => Some((l.id, l.name, l.highlightClass, l.extension, l.cpuFactor, l.memFactor, l.comment, l.active)))
+    )((name, highlightClass, extension, cpuFactor, memFactor, comment, active) => Language("", name, highlightClass, extension, cpuFactor, memFactor, comment, active))
+    ((l: Language) => Some((l.name, l.highlightClass, l.extension, l.cpuFactor, l.memFactor, l.comment, l.active)))
   )
 }
